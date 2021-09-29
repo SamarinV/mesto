fd1eecca59b751a628f39cbe3e5dcb609f5cdd79
Vladimir Samarin <v.a.samarin@yandex.ru>
2021-09-29 20:16:23 +0300

    сделать попап и лайки

--- index.js
+++ index.js
@@ -13,17 +13,22 @@ buttonClose.addEventListener('click', function(evt){
   form.classList.add('pop-up_disabled');
 });
 
-
-let formName = document.querySelector('.pop-up__name');
-let formAbout = document.querySelector('.pop-up__about-self');
-let buttonSave = document.querySelector('.pop-up__save');
+//pop-up
 let Name = document.querySelector('.profile__name');
 let About = document.querySelector('.profile__about-self');
-
 form.addEventListener('submit' , function(evt){
   evt.preventDefault()
+  let formName = document.querySelector('.pop-up__name');
+  let formAbout = document.querySelector('.pop-up__about-self');
   Name.innerText = formName.value;
   About.innerText = formAbout.value;
   form.classList.add('pop-up_disabled');
+});
 
+//
+let like = document.querySelectorAll('.places__like');    //меняем лайк
+Array.prototype.slice.call(like).forEach(el => {
+  el.addEventListener('click', e => {
+      el.classList.toggle('places__like_active');
+  });
 });
