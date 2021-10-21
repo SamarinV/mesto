902dfb6c526afa128cef8817c2203d2156d99c5a
VladimirS <v.a.samarin@yandex.ru>
2021-10-21 11:03:36 +0300

    сделать закрытие попапов при клике вне области их форм

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,9 @@ const formAddPlace = document.querySelector('.pop-up__form_add-place');
 const boxForPlaces = document.querySelector('.places');
 
 const newNamePlace = document.querySelector('.pop-up__input-text_name-place');
+const mainNamePlacePlaceholder = newNamePlace.placeholder; //placeholder имени нового места
 const newImageUrl = document.querySelector('.pop-up__input-text_url-image');
+const mainImageUrlPlaceholder = newImageUrl.placeholder; //placeholder адреса картинки нового места
 const placeTemplate = document.querySelector('.places__template').content;
 
 const popupLookPlace = document.querySelector('.pop-up_position_look-place');
@@ -22,25 +24,51 @@ const buttonCloseLookPlace = document.querySelector('.pop-up__close_look-place')
 const imageLookPlace = document.querySelector('.pop-up__image');
 const imageTitleLookPlace = document.querySelector('.pop-up__image-description');
 
+//--------------------------------------------------------------------
+//функционал placeholder у инпутов попап при фокусе
 
+//инпут с именем для нового места
+newNamePlace.addEventListener('focus', (e) => {
+	newNamePlace.placeholder = '';
+});
+newNamePlace.addEventListener('blur', (e) => {
+	newNamePlace.placeholder = mainNamePlacePlaceholder;
+});
+//инпут с адресом картинки для нового места
+newImageUrl.addEventListener('focus', (e) => {
+	newImageUrl.placeholder = '';
+});
+newImageUrl.addEventListener('blur', (e) => {
+	newImageUrl.placeholder = mainImageUrlPlaceholder;
+});
+
+//---------------------------------------------------------------------
+//общий функционал для попапов
+
+//открытие попапа
 function openPopup(el){
 	el.classList.add('pop-up_open');
 };
+//закрытие попапа
 function closePopup(el){
 		el.classList.remove('pop-up_open');
 };
 
+//---------------------------------------------------------------------
+//функционал редактирования профиля
 
 buttonEdit.addEventListener('click', () => {
 	newProfileName.value = profileName.textContent;
 	newProfileAboutSelf.value = profileAboutSelf.textContent;
 	openPopup(popupEdit);
 });
-
-buttonCloseEdit.addEventListener('click', () => {
-	closePopup(popupEdit);
+//закрыть редактирование профиля по нажатию на крестик или вне блока с формой
+	popupEdit.addEventListener('click', (event) => {
+    if ( !event.target.closest('.pop-up__container') || event.target === buttonCloseEdit) {
+        closePopup(popupEdit);
+    };
 });
-
+//сохранить профиль и закрыть попап редактирования
 formEditProfile.addEventListener('submit', (event) => {
 	event.preventDefault();
 	profileName.textContent = newProfileName.value;
@@ -48,13 +76,17 @@ formEditProfile.addEventListener('submit', (event) => {
 	closePopup(popupEdit);
 });
 
+//-----------------------------------------------------------------------
+//функционал добавления новых мест
 
+//создание нового места
 function createNewPlace(item) {
 	const placeElement = placeTemplate.querySelector('.places__item').cloneNode(true);
 	const placeImage = placeElement.querySelector('.places__image');
 	const placeLike = placeElement.querySelector('.places__like');
 	placeImage.setAttribute('src', item.link);
 	placeImage.setAttribute('alt', item.name);
+	//функционал открытия попапа просмотра места по событию клика на картинку
 	placeImage.addEventListener('click', () => {
 		imageLookPlace.setAttribute('src', item.link);
 		imageLookPlace.setAttribute('alt', item.name)
@@ -62,35 +94,42 @@ function createNewPlace(item) {
 		openPopup(popupLookPlace);
 	});
 	placeElement.querySelector('.places__title').textContent = item.name;
+	//функционал для лайков
 	placeLike.addEventListener('click', () => {
 		placeLike.classList.toggle('places__like_active');
 	}); 
+	//функционал для удаления места
 	placeElement.querySelector('.places__delete').addEventListener('click', (event) => {
 		event.target.closest('.places__item').remove();
 	});
 	return placeElement; 
 }
-
+//очистка формы добавленя нового места
 function clearFormPlace(){
 	newNamePlace.value = '';
 	newImageUrl.value = '';
 };
-
+//добавление новой карточки мест в начало
 function appendPlace(item){
 	boxForPlaces.prepend(createNewPlace(item));
 }
-
+//перебор массива мест из ./scripts/arrayPlaces.js и их добавление
 arrayPlaces.forEach(appendPlace);
 
+//кнопка открыть попап нового места
 buttonAddPlace.addEventListener('click', () => {
 	openPopup(popupAddPlace);
 });
 
-buttonCloseAddPlace.addEventListener('click', () => {
-	closePopup(popupAddPlace);
-	clearFormPlace();
+//закрыть попап нового места при клике на крестик или вне области формы
+popupAddPlace.addEventListener('click', (event) => {
+    if ( !event.target.closest('.pop-up__container') || event.target === buttonCloseAddPlace) {
+        closePopup(popupAddPlace);
+			 	clearFormPlace();
+    };
 });
 
+//кнопка сохранить новое место
 formAddPlace.addEventListener('submit', (event) => {
 	event.preventDefault();
 	const item = {
@@ -102,6 +141,12 @@ formAddPlace.addEventListener('submit', (event) => {
 	clearFormPlace();
 });
 
-buttonCloseLookPlace.addEventListener('click', () => {
-	closePopup(popupLookPlace);
+//-----------------------------------------------------------
+//функционал просмотра мест
+//(открытие реализовано в createNewPlace)
+//закрыть попап при клике на крестик или вне области формы
+popupLookPlace.addEventListener('click', (event) => {
+    if ( !event.target.closest('.pop-up__container') || event.target === buttonCloseLookPlace) {
+        closePopup(popupLookPlace);
+    };
 });
