b8f22aa7689c873e1e736af7ab32abc46f9371b4
VladimirS <v.a.samarin@yandex.ru>
2021-10-27 17:24:30 +0300

    рефакторинг/старт работы по валидации над всеми формами

--- scripts/index.js
+++ scripts/index.js
@@ -14,9 +14,9 @@ const formAddPlace = document.querySelector('.pop-up__form_add-place');
 const boxForPlaces = document.querySelector('.places');
 
 const newNamePlace = document.querySelector('.pop-up__input-text_name-place');
-const mainNamePlacePlaceholder = newNamePlace.placeholder; //placeholder имени нового места
+const mainNamePlacePlaceholder = newNamePlace.placeholder;
 const newImageUrl = document.querySelector('.pop-up__input-text_url-image');
-const mainImageUrlPlaceholder = newImageUrl.placeholder; //placeholder адреса картинки нового места
+const mainImageUrlPlaceholder = newImageUrl.placeholder;
 const placeTemplate = document.querySelector('.places__template').content;
 
 const popupLookPlace = document.querySelector('.pop-up_position_look-place');
@@ -25,8 +25,118 @@ const imageLookPlace = document.querySelector('.pop-up__image');
 const imageTitleLookPlace = document.querySelector('.pop-up__image-description');
 
 //--------------------------------------------------------------------
-//функционал placeholder у инпутов попап при фокусе
 
+
+//---------------------------------------------------------------------
+//общий функционал для попапов
+
+//открытие попапа
+function openPopup(el){
+	el.classList.add('pop-up_open');
+}
+//закрытие попапа
+function closePopup(el){
+		el.classList.remove('pop-up_open');
+}
+//закрытие по escape
+const keyHandler = (el) => {
+	document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape'){
+		if(el === popupAddPlace){
+			closePopup(el);
+			clearFormPlace();
+		}
+		else closePopup(el);
+	}
+});
+}
+//---------------------------------------------------------------------
+//функционал редактирования профиля
+//открытие по клику
+buttonEdit.addEventListener('click', () => {
+	newProfileName.value = profileName.textContent;
+	newProfileAboutSelf.value = profileAboutSelf.textContent;
+	openPopup(popupEdit);
+	//закрытие по escape
+	keyHandler(popupEdit);
+});
+//закрыть редактирование профиля по нажатию на крестик или на оверлей
+	popupEdit.addEventListener('click', (e) => {
+		if ( !e.target.closest('.pop-up__container') || e.target === buttonCloseEdit) {
+			closePopup(popupEdit);
+		}
+});
+//сохранить профиль и закрыть попап редактирования
+formEditProfile.addEventListener('submit', (e) => {
+	e.preventDefault();
+	profileName.textContent = newProfileName.value;
+	profileAboutSelf.textContent = newProfileAboutSelf.value;
+	closePopup(popupEdit);
+});
+
+//-----------------------------------------------------------------------
+//добавление новых карточек, попап addPlace,  попап lookPlace
+
+//создание новой карточки
+function createNewPlace(item) {
+	const placeElement = placeTemplate.querySelector('.places__item').cloneNode(true);
+	const placeImage = placeElement.querySelector('.places__image');
+	placeImage.setAttribute('src', item.link);
+	placeImage.setAttribute('alt', item.name);
+	placeElement.querySelector('.places__title').textContent = item.name;
+	return placeElement;
+}
+
+//добавление новой карточки в начало
+function appendPlace(item){
+	boxForPlaces.prepend(createNewPlace(item));
+}
+
+//перебор массива мест из ./scripts/arrayPlaces.js и их добавление
+arrayPlaces.reverse().forEach(appendPlace);
+
+//очистка формы попапа addPlace
+function clearFormPlace(){
+	newNamePlace.value = '';
+	newImageUrl.value = '';
+}
+
+//слушаем в карточках лайки, удаление и открытие попапа lookPlace
+boxForPlaces.addEventListener('click', (e) => {
+	//лайки
+	if (e.target.classList.contains('places__like')){
+		e.target.classList.toggle('places__like_active');
+	};
+	//удаление
+	if (e.target.classList.contains('places__delete')){
+		e.target.closest('.places__item').remove();
+	};
+	//открытие попапа lookPlace
+	if (e.target.classList.contains('places__image')){
+		imageLookPlace.setAttribute('src', e.target.attributes[1].nodeValue);
+		imageLookPlace.setAttribute('alt', e.target.alt);
+		imageTitleLookPlace.textContent = e.target.alt;
+		openPopup(popupLookPlace);
+		//слушаем его закрытие по Escape
+		keyHandler(popupLookPlace);
+	};
+});
+
+//закрыть попап lookPlace при клике на кнопку закрыть или на оверлей
+popupLookPlace.addEventListener('click', (e) => {
+	if ( !e.target.closest('.pop-up__container') || e.target === buttonCloseLookPlace) {
+		closePopup(popupLookPlace);
+	}
+});
+
+//открыть попап addPlace по кнопке
+buttonAddPlace.addEventListener('click', () => {
+	openPopup(popupAddPlace);
+	//слушаем закрытие по Escape
+	keyHandler(popupAddPlace);
+});
+
+//placeholder у инпутов addPlace при фокусе
 //инпут с именем для нового места
 newNamePlace.addEventListener('focus', () => {
 	newNamePlace.placeholder = '';
@@ -42,101 +152,17 @@ newImageUrl.addEventListener('blur', () => {
 	newImageUrl.placeholder = mainImageUrlPlaceholder;
 });
 
-//---------------------------------------------------------------------
-//общий функционал для попапов
-
-//открытие попапа
-function openPopup(el){
-	el.classList.add('pop-up_open');
-}
-//закрытие попапа
-function closePopup(el){
-		el.classList.remove('pop-up_open');
-}
-
-//---------------------------------------------------------------------
-//функционал редактирования профиля
-
-buttonEdit.addEventListener('click', () => {
-	newProfileName.value = profileName.textContent;
-	newProfileAboutSelf.value = profileAboutSelf.textContent;
-	openPopup(popupEdit);
-});
-//закрыть редактирование профиля по нажатию на крестик или вне блока с формой
-	popupEdit.addEventListener('click', (event) => {
-    if ( !event.target.closest('.pop-up__container') || event.target === buttonCloseEdit) {
-        closePopup(popupEdit);
-    }
-});
-//сохранить профиль и закрыть попап редактирования
-formEditProfile.addEventListener('submit', (event) => {
-	event.preventDefault();
-	profileName.textContent = newProfileName.value;
-	profileAboutSelf.textContent = newProfileAboutSelf.value;
-	closePopup(popupEdit);
-});
-
-//-----------------------------------------------------------------------
-//функционал добавления новых мест
-
-//создание нового места
-function createNewPlace(item) {
-	const placeElement = placeTemplate.querySelector('.places__item').cloneNode(true);
-	const placeImage = placeElement.querySelector('.places__image');
-	placeImage.setAttribute('src', item.link);
-	placeImage.setAttribute('alt', item.name);
-	placeElement.querySelector('.places__title').textContent = item.name;
-	return placeElement;
-}
-
-//функционал для карточек
-boxForPlaces.addEventListener('click', (evt) => {
-	//лайки
-	if (evt.target.classList.contains('places__like')){
-		evt.target.classList.toggle('places__like_active');
-	};
-	//удаление
-	if (evt.target.classList.contains('places__delete')){
-		evt.target.closest('.places__item').remove();
-	};
-	//открытие попапа просмотра места по клику на картинку
-	if (evt.target.classList.contains('places__image')){
-		console.log(evt);
-		imageLookPlace.setAttribute('src', evt.target.attributes[1].nodeValue);
-		imageLookPlace.setAttribute('alt', evt.target.alt);
-		imageTitleLookPlace.textContent = evt.target.alt;
-		openPopup(popupLookPlace);
-	};
-});
-
-//очистка формы добавленя нового места
-function clearFormPlace(){
-	newNamePlace.value = '';
-	newImageUrl.value = '';
-}
-//добавление новой карточки мест в начало
-function appendPlace(item){
-	boxForPlaces.prepend(createNewPlace(item));
-}
-//перебор массива мест из ./scripts/arrayPlaces.js и их добавление
-arrayPlaces.reverse().forEach(appendPlace);
-
-//кнопка открыть попап нового места
-buttonAddPlace.addEventListener('click', () => {
-	openPopup(popupAddPlace);
-});
-
-//закрыть попап нового места при клике на крестик или вне области формы
-popupAddPlace.addEventListener('click', (event) => {
-    if ( !event.target.closest('.pop-up__container') || event.target === buttonCloseAddPlace) {
-        closePopup(popupAddPlace);
+//закрыть попап addPlace при клике на кнопку закрыть или на оверлей
+popupAddPlace.addEventListener('click', (e) => {
+		if ( !e.target.closest('.pop-up__container') || e.target === buttonCloseAddPlace) {
+			closePopup(popupAddPlace);
 			 	clearFormPlace();
-    }
+		}
 });
 
-//кнопка сохранить новое место
-formAddPlace.addEventListener('submit', (event) => {
-	event.preventDefault();
+//кнопка сохранить новое место в попапе addPlace
+formAddPlace.addEventListener('submit', (e) => {
+	e.preventDefault();
 	const item = {
 		name: newNamePlace.value,
 		link: newImageUrl.value
@@ -146,12 +172,4 @@ formAddPlace.addEventListener('submit', (event) => {
 	clearFormPlace();
 });
 
-//-----------------------------------------------------------
-//функционал просмотра мест
-//(открытие реализовано в createNewPlace)
-//закрыть попап при клике на крестик или вне области формы
-popupLookPlace.addEventListener('click', (event) => {
-    if ( !event.target.closest('.pop-up__container') || event.target === buttonCloseLookPlace) {
-        closePopup(popupLookPlace);
-    }
-});
+
