60392daccc3df4b07528ee9c62634e2e9ffaa457
VladimirS <v.a.samarin@yandex.ru>
2021-10-28 20:34:16 +0300

    рефакторинг validate.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,40 +1,5 @@
-//--------------------------------------------------------------------------
-// инпуты
 
-const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-}; 
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-	errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  errorElement.classList.remove('popup__input-error_active');
-  errorElement.textContent = '';
-}; 
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-	const buttonElement = formElement.querySelector('.popup__save');
-  inputList.forEach((inputElement) => {
-		//состояние кнопки сохранить/добавить для выбранной формы
-		toggleButtonState(inputList, buttonElement);
-		//убираем надпись error
-    hideInputError(formElement, inputElement);
-    inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement)
-			toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
+//ищем выбранную форму для валидации через массив всех форм
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.popup__form'));
   formList.forEach((formElement) => {
@@ -45,6 +10,44 @@ const enableValidation = () => {
   });
 };
 
+//ищем инпуты выбранной формы через массив инпутов для каждой формы для дальнейшей валидации
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+	const buttonElement = formElement.querySelector('.popup__save');
+  inputList.forEach((inputElement) => {
+		//состояние кнопки сохранить/добавить для выбранной формы
+		toggleButtonState(inputList, buttonElement);
+		//убираем надпись error(нужно при открытии попапа)
+    hideInputError(formElement, inputElement);
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement)
+			toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+// валидация input
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}; 
+
+//находим span к каждому input и показываем error
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+	errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+//находим span к каждому инпуту и прячем error
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+}; 
 
 // меняем кнопку submit у форм
 function toggleButtonState (inputList, buttonElement){
@@ -55,6 +58,7 @@ function toggleButtonState (inputList, buttonElement){
 } 
 }
 
+//валидация кнопки
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
   	return !inputElement.validity.valid;
