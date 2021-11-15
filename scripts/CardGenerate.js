561950bef3ce49b04bec7268a75105e376bd7e3c
VladimirS <v.a.samarin@yandex.ru>
2021-11-15 18:08:18 +0300

    1

--- scripts/CardGenerate.js
+++ scripts/CardGenerate.js
@@ -1,4 +1,6 @@
-class Card{
+
+
+export class Card{
 	constructor(element, cardSelector){
 		this._name = element.name;
 		this._link = element.link;
@@ -10,19 +12,6 @@ class Card{
     return cardElement;
   }
 
-	_handleOpenPopup() {
-		imageLookPlace.setAttribute('src', `${this._link}`);
-		imageLookPlace.setAttribute('alt', this._name);
-		imageTitleLookPlace.textContent = this._name;
-		openPopup(popupLookPlace);
-
-  }
-  _handleClosePopup() {
-		closePopup(popupLookPlace);
-		imageLookPlace.removeAttribute('src');
-		imageLookPlace.removeAttribute('alt');
-		imageTitleLookPlace.textContent = '';
-  }
 	_handleDeleteCard() {
 		this._element.remove();
 	}
@@ -31,20 +20,14 @@ class Card{
 	}
 
 	_setEventListeners() {
-    this._element.querySelector('.places__image').addEventListener('click', () => {
-      this._handleOpenPopup();
-    });
+
 		this._element.querySelector('.places__delete').addEventListener('click', () => {
 			this._handleDeleteCard()
 		});
 		this._element.querySelector('.places__like').addEventListener('click', () => {
 			this._handleToggleLike();
 		});
-		this._element.querySelector('.popup_position_look-place').addEventListener('click', (e) => {
-			if ( !e.target.closest('.popup__container') || e.target === buttonCloseLookPlace) {
-				this._handleClosePopup()
-			}
-		});
+
   }
 	generateCard() {
     this._element = this._getTemplate();
