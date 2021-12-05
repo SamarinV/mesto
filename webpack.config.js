05c8b847035dfb25ed4505d3a4078dfc7e4c663d
VladimirS <v.a.samarin@yandex.ru>
2021-12-05 16:09:53 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,74 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		globalObject: "this",
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: './src/index.html',
-		}),
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin(),
-	],
-	optimization: {
-		minimizer: [
-			new CssMinimizerPlugin(),
-		],
-	},
-	// devtool: "source-map",
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist'),
+module.exports = (env, args) => {
+	const map = args.mode === 'production' ? [
+		MiniCssExtractPlugin.loader, {
+			loader: "css-loader",
+			options: {
+				importLoaders: 1
+			}
 		},
-		compress: true,
-		port: 9000,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ico|gif|png|jpg|jpeg|svg)$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'images/[hash][ext][query]'
-				}
+		"postcss-loader"
+	] : [
+		MiniCssExtractPlugin.loader, "css-loader"
+	]
+	return {
+		entry: './src/index.js',
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		devtool: args.mode === 'development' ? 'source-map' : false,
+		plugins: [
+			new HTMLWebpackPlugin({
+				template: './src/index.html',
+			}),
+			new MiniCssExtractPlugin(),
+		],
+		devServer: {
+			static: {
+				directory: path.join(__dirname, 'dist'),
 			},
-			{
-				test: /\.(woff(2)?|ttf|eot||otf)$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'fonts/[hash][ext][query]'
-				}
-			},
+			compress: true,
+			port: 9000,
+		},
+		module: {
+			rules: [
 
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, {
-					loader: "css-loader",
-					options: {
-						importLoaders: 1
+				{
+					test: /\.js$/,
+					exclude: /(node_modules)/,
+					use: {
+						loader: 'babel-loader',
 					}
 				},
-					"postcss-loader"
-				]
-			},
+				{
+					test: /\.(ico|gif|png|jpg|jpeg|svg)$/,
+					type: 'asset/resource',
+					generator: {
+						filename: 'images/[hash][ext][query]'
+					}
+				},
+				{
+					test: /\.(woff(2)?|ttf|eot||otf)$/,
+					type: 'asset/resource',
+					generator: {
+						filename: 'fonts/[hash][ext][query]'
+					}
+				},
 
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-				}
-			},
+				{
+					test: /\.css$/,
+					use: map
+				},
 
-		],
-	},
+
+			],
+		},
+	}
 }
